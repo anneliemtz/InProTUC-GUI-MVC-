e7a6fe947ecf19e34c37618e934fd90e367774e5
anneliemtz <anneliemar@gmail.com>
2016-06-06 16:52:21 +0200

    Für die Button-Spalte

---
+++ src/View/Renderer/JButtonEditor.java
@@ -0,0 +1,16 @@
+package View.Renderer;
+
+import javax.swing.table.TableCellEditor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.AbstractCellEditor;
+import javax.swing.JTable;
+
+/**
+ * Created by annelie on 02.06.16.
+ */
+public class JButtonEditor {
+    //TODO
+}
